2c6325fde72e87cb317290d2f5c164bdbb15a420
Christia T <christiatidwell@bellsouth.net>
2018-08-22 01:50:05 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -8,7 +8,7 @@ class Actor < ActiveRecord::Base
 
   def list_roles
     self.character.map do |join|
-      "#{join.character.name} - #{join.show.name}
+      "#{join.character.name} - #{join.show.name}"
     end
   end
 
